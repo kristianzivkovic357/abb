0805c8a5fdd07fb1c730a214b5500549a7d0bb98
Kristian Zivkovic <Kristian Zivkovic>
2017-06-05 21:45:26 +0200

    update

--- pushNotifications.js
+++ pushNotifications.js
@@ -1,4 +1,4 @@
-var FCM = require('fcm-node');
+/*var FCM = require('fcm-node');
 var SERVER_KEY='AAAAPwFS6QU:APA91bFdrfiFbwIhGezBWXMNuJHRmu4zucXHZXV7I2C6REx3RaveZ5suM7JoOEEp3qEa-LfLHajlwlHskmDoHSuLFwxdIgiOae3f_dWqt7S6U9gfg3WsE-g9qjrv2GSdbNWv-9qSt8h1';
 var fcm = new FCM(SERVER_KEY);
 
@@ -30,3 +30,4 @@ function sendNotification(deviceID,message)
         }
     });
 }
+*/
